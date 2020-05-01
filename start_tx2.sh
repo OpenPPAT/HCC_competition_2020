68b5bb2b6e39dd714ab043edc41e31fa56406232
huangjuite <huangjuite@gmail.com>
2020-05-01 14:36:04 +0800

    script

--- start_tx2.sh
+++ start_tx2.sh
@@ -6,4 +6,4 @@ WLAN0_IP=$(ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 source ~/HCC_competition_2020/environment.sh
 export ROS_IP=10.42.0.1
 
-roslaunch teleop_twist_joy teleop.launch eth0:=$ETH0_IP wlo0:=$WLAN0_IP
+roslaunch teleop_twist_joy teleop.launch eth0:=$ETH0_IP wlan0:=$WLAN0_IP
